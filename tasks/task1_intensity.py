ebacb1ac60f1a4de84a25479a6d9f2c0c50e0b82
Pabasara715 <pabasaravihanga715@gmail.com>
2025-06-19 23:34:31 +0530

    Add function to reduce intensity levels in grayscale images

---
+++ tasks/task1_intensity.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def reduce_intensity_levels(image, levels):
+    """Reduces the number of intensity levels in a grayscale image."""
+    # (Copy the function code from the previous answer here)
+    if not (levels > 1 and (levels & (levels - 1) == 0)):
+        raise ValueError("Number of levels must be an integer power of 2 greater than 1.")
+    factor = 256 // levels
+    new_value_multiplier = 255 // (levels - 1)
+    reduced_image = (image // factor) * new_value_multiplier
+    return reduced_image.astype(np.uint8)
